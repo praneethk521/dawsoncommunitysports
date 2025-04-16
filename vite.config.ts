c376f4ee2da81d149139907993c0cb0cfcd6f248
Praneeth Perna <praneeth.perna@veeva.com>
2025-04-16 17:00:49 -0400

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/', // ✅ root path for custom domain
   plugins: [react()],
 });
