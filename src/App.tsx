e86493acf4d6c6de3b3d3a5307eace5aa6dd23f4
Praneeth Perna <praneeth.perna@veeva.com>
2025-04-16 16:54:47 -0400

    Initial deploy

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="text-center mt-20 text-2xl text-blue-800 font-bold">
+      Dawson Community Sports 2025 Coming Soon!
+    </div>
+  );
+};
+
+export default App;
